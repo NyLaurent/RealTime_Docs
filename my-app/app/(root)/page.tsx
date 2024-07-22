09b65f9d5518d5df47297ceaab2700352f004c43
lauLanez <laulanyumbayire@gmail.com>
2024-07-22 13:04:48 -0500

    done with header section

--- my-app/app/(root)/page.tsx
+++ my-app/app/(root)/page.tsx
@@ -1,11 +1,13 @@
 
 import { Button } from '@/components/ui/button'
 import {Editor} from '@/components/editor/Editor'
+import Header from '@/components/Header'
 
 
 const Home = () => {
   return (
     <div>
+      <Header/>
       <Editor/>
     </div>
   )
