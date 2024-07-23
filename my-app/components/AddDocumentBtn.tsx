66beb93017a585f4f167ddf03fb823071b52d530
lauLanez <laulanyumbayire@gmail.com>
2024-07-23 12:38:57 -0600

    added a router

--- my-app/components/AddDocumentBtn.tsx
+++ my-app/components/AddDocumentBtn.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 import { Button } from './ui/button';
 import Image from 'next/image';
 import { createDocument } from '@/lib/actions/room.actions';
+import { useRouter } from 'next/router';
+
 
 interface AddDocumentBtnProps {
   userId: string;
@@ -11,33 +13,43 @@ interface AddDocumentBtnProps {
 }
 
 const AddDocumentBtn = ({ userId, email }: AddDocumentBtnProps) => {
-  const addDocumentHandler = async () => {
-    try {
-      createDocument({userId,email})
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    const router = useRouter();
 
-  return (
-    <div>
-      <Button 
-        type='submit' 
-        onClick={addDocumentHandler}
-        className='gradient-blue flex gap-1 shadow-md'
-      >
-        <Image 
-          src='/assets/icons/add.svg'
-          alt='add'
-          width={24}
-          height={24}
-        />
-        <p className='hidden sm:block'>
-          Start a blank document
-        </p>
-      </Button>
-    </div>
-  );
+
+   
+    
+    const addDocumentHandler = async () => {
+        
+        
+        try {
+            const room = await createDocument({ userId, email });
+            if (room) {
+                router.push(`/documents/${room.id}`);
+            }
+        } catch (error) {
+            console.error('Error creating document:', error);
+        }
+    };
+
+    return (
+        <div>
+            <Button 
+                type='submit'  // 'button' type for a non-form button
+                onClick={addDocumentHandler}
+                className='gradient-blue flex gap-1 shadow-md'
+            >
+                <Image 
+                    src='/assets/icons/add.svg'
+                    alt='add'
+                    width={24}
+                    height={24}
+                />
+                <p className='hidden sm:block'>
+                    Start a blank document
+                </p>
+            </Button>
+        </div>
+    );
 };
 
 export default AddDocumentBtn;
